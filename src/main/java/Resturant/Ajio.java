5b2d557a7cbb68f589d1fc3666b894928b35ae81
Manoj194 <manuvirat772@gmail.com>
2022-11-22 17:08:01 +0530

    commit1

--- src/main/java/Resturant/Ajio.java
+++ src/main/java/Resturant/Ajio.java
@@ -1,12 +1,7 @@
 package Resturant;
 
-import java.sql.Driver;
-
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,7 +11,8 @@ public class Ajio {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
+		driver.
+		manage().window().maximize();
 		driver.get("https://www.ajio.com/?gclid=EAIaIQobChMI8LaO6Zfh-QIVa5VLBR2nsQq3EAAYASAAEgJbRfD_BwE");
 		Thread.sleep(2000);
 		//Alert a = driver.switchTo().alert();
